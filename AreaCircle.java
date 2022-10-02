05887a74c0b79a79a2ff9f6e344244abc29d0cee
Akshada <autiakshada2002@gmail.com>
2022-10-02 12:46:15 +0530

    added AreaCircle.java file

---
+++ AreaCircle.java
@@ -0,0 +1,16 @@
+//Write a java program to calculate the area of circle
+import java.util.*;
+public class AreaCircle {
+    public static void main(String args[])
+    {
+        double r, area;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the radius of circle ");
+        r=in.nextDouble();
+        area=3.14*r*r;
+        System.out.println("Area of a circle is "+area);
+
+
+    }
+}
+
