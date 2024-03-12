f3e3b0fdc2a500cb695fe8c2ac27a179e52c95a3
Akshada <autiakshada2002@gmail.com>
2024-03-12 13:50:04 +0530

    Exception code

---
+++ ArrayList/D4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class D4
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc= new Scanner(System.in);
+		int num1=sc.nextInt();//50
+		int sum=num1+num2;
+		System.out.println(sum);
+	}
+}
